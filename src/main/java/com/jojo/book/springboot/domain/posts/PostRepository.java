a70942b438ea68684c1a7d9c10525a260b1e5787
gbyman <dongchan-2@hanmail.net>
2020-08-12 20:09:42 +0900

    프로젝트에 Spring Data Jpa 적용 및 테스트 코드 추가

---
+++ src/main/java/com/jojo/book/springboot/domain/posts/PostRepository.java
@@ -0,0 +1,6 @@
+package com.jojo.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+}
