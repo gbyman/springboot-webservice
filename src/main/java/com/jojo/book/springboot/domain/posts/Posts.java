dba74cd88f5c3bcad01a0bc7a369fd4c6d9dbb19
gbyman <dongchan-2@hanmail.net>
2020-08-14 19:20:23 +0900

    JPA Auditing 관한 설정 추가, 등록/수정 시간 자동화 기능 구현

--- src/main/java/com/jojo/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojo/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojo.book.springboot.domain.posts;
 
+import com.jojo.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK필드
     @GeneratedValue(strategy = GenerationType.IDENTITY)
